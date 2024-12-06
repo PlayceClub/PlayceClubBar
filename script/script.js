22932a819cb2aadc43bca676e5d19476bc5ddbef
PlayceClub <testeromoment@gmail.com>
2024-12-06 06:59:41 +0300

    script.js

--- script/script.js
+++ script/script.js
@@ -142,7 +142,7 @@ document.addEventListener("DOMContentLoaded", () => {
     /*** Отправка заказа в Telegram ***/
     async function sendOrderToTelegram() {
         const token = "7978127151:AAEiJVWSEmrXn6pj26O3C8HrSNVmKZYKyDA"; // Замените на токен вашего бота
-        const chatId = "1776219693";  // Замените на ваш chat_id
+        const chatId = "-1002430027699";  // Замените на ваш chat_id
 
         let message = "🛒 *Ваш заказ:*\n";
         let total = 0;
